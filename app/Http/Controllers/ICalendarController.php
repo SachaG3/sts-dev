363e5d2f263055c9db14d28afef6aa264d607d88
Sacha Guignard <sacha.guignard@caensup.fr>
2024-08-28 23:07:04 +0200

    finish

--- app/Http/Controllers/ICalendarController.php
+++ app/Http/Controllers/ICalendarController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Semaine;
 use Carbon\Carbon;
 use Carbon\CarbonPeriod;
 use Illuminate\Http\Request;
@@ -10,12 +11,11 @@ use Spatie\IcalendarGenerator\Components\Event;
 
 class ICalendarController extends Controller
 {
-    protected $edtController;
     protected $courseWeeks;
+    protected $detailedWeekAdded = false;
 
-    public function __construct(EdtController $edtController)
+    public function __construct()
     {
-        $this->edtController = $edtController;
         $this->courseWeeks = [
             '2024-08-26', '2024-09-09', '2024-09-30', '2024-10-21', '2024-11-04',
             '2024-11-25', '2024-12-09', '2025-01-06', '2025-01-27', '2025-02-17',
@@ -30,18 +30,6 @@ class ICalendarController extends Controller
             ->refreshInterval(60)
             ->productIdentifier('//Votre École//STS Dev Calendrier Alternance//FR');
 
-        $edtData = $this->edtController->getData()->getContent();
-        $data = json_decode($edtData, true);
-
-        $this->addAllWeeksToCalendar($calendar, $data);
-
-        return response($calendar->get())
-            ->header('Content-Type', 'text/calendar; charset=utf-8')
-            ->header('Content-Disposition', 'attachment; filename="sts-dev-calendar-alternance.ics"');
-    }
-
-    private function addAllWeeksToCalendar($calendar, $data)
-    {
         $startDate = Carbon::parse('2024-08-26');
         $endDate = Carbon::parse('2025-08-31');
 
@@ -49,47 +37,40 @@ class ICalendarController extends Controller
 
         foreach ($weeks as $weekStart) {
             if (in_array($weekStart->format('Y-m-d'), $this->courseWeeks)) {
-                $this->addCourseWeek($calendar, $weekStart, $data);
+                $this->addCourseWeek($calendar, $weekStart);
             } else {
                 $this->addAlternanceWeek($calendar, $weekStart);
             }
         }
+
+        return response($calendar->get())
+            ->header('Content-Type', 'text/calendar; charset=utf-8')
+            ->header('Content-Disposition', 'attachment; filename="sts-dev-calendar-alternance.ics"');
     }
 
-    private function addCourseWeek($calendar, $weekStart, $data)
+    private function addCourseWeek($calendar, $weekStart)
     {
-        if ($data && isset($data['emploi_du_temps']) && $this->isWeekDataAvailable($data, $weekStart)) {
-            $this->addActualCourseWeek($calendar, $weekStart, $data);
+        $semaine = Semaine::latest()->first();
+
+        if ($semaine && $semaine->json_data && !$this->detailedWeekAdded) {
+            $data = json_decode($semaine->json_data, true);
+            $this->addDetailedCourseWeek($calendar, $weekStart, $data);
+            $this->detailedWeekAdded = true;
         } else {
             $this->addDefaultCourseWeek($calendar, $weekStart);
         }
     }
 
-    private function isWeekDataAvailable($data, $weekStart)
-    {
-        if (!isset($data['emploi_du_temps'][0]['date'])) {
-            return false;
-        }
-        $dataWeekStart = Carbon::createFromFormat('d/m/Y', $data['emploi_du_temps'][0]['date'])->startOfWeek();
-        return $dataWeekStart->eq($weekStart);
-    }
-
-    private function addActualCourseWeek($calendar, $weekStart, $data)
+    private function addDetailedCourseWeek($calendar, $weekStart, $data)
     {
         foreach ($data['emploi_du_temps'] as $index => $jourData) {
-            if (isset($jourData['jour'])) {
-                $dayOfWeek = $this->getDayOfWeek($jourData['jour']);
-            } else {
-                $dayOfWeek = $index + 1; // Assuming the array starts with Monday
-            }
-
-            $currentDate = $weekStart->copy()->addDays($dayOfWeek - 1);
+            $date = $weekStart->copy()->addDays($index)->setTimezone('Europe/Paris');
 
             if (isset($jourData['cours']) && is_array($jourData['cours'])) {
                 foreach ($jourData['cours'] as $coursData) {
                     if (isset($coursData['heure_debut']) && isset($coursData['heure_fin']) && isset($coursData['matiere'])) {
-                        $start = $currentDate->copy()->setTimeFromTimeString($this->formatTime($coursData['heure_debut']));
-                        $end = $currentDate->copy()->setTimeFromTimeString($this->formatTime($coursData['heure_fin']));
+                        $start = $date->copy()->setTimeFromTimeString($this->formatTime($coursData['heure_debut']));
+                        $end = $date->copy()->setTimeFromTimeString($this->formatTime($coursData['heure_fin']));
 
                         $description = "";
                         if (isset($coursData['professeur'])) {
@@ -98,10 +79,11 @@ class ICalendarController extends Controller
                         if (isset($coursData['salle'])) {
                             $description .= "Salle: {$coursData['salle']}";
                         }
+                        $description = trim($description);
 
                         $event = Event::create()
                             ->name($coursData['matiere'])
-                            ->description(trim($description))
+                            ->description($description)
                             ->startsAt($start)
                             ->endsAt($end);
 
@@ -110,21 +92,6 @@ class ICalendarController extends Controller
                 }
             }
         }
-    }
-
-    private function getDayOfWeek($jour)
-    {
-        $jours = [
-            'lundi' => 1,
-            'mardi' => 2,
-            'mercredi' => 3,
-            'jeudi' => 4,
-            'vendredi' => 5,
-            'samedi' => 6,
-            'dimanche' => 7
-        ];
-
-        return $jours[strtolower($jour)] ?? 1; // Default to Monday if unknown
     }
 
     private function formatTime($time)
@@ -137,14 +104,14 @@ class ICalendarController extends Controller
 
     private function addDefaultCourseWeek($calendar, $weekStart)
     {
-        for ($dayOffset = 0; $dayOffset < 5; $dayOffset++) {
-            $currentDate = $weekStart->copy()->addDays($dayOffset);
+        for ($i = 0; $i < 5; $i++) {
+            $date = $weekStart->copy()->addDays($i)->setTimezone('Europe/Paris');
 
             $event = Event::create()
                 ->name('En cours')
                 ->description('Semaine de formation')
-                ->startsAt($currentDate->copy()->setTime(8, 0))
-                ->endsAt($currentDate->copy()->setTime(17, 0));
+                ->startsAt($date->copy()->setTime(8, 0))
+                ->endsAt($date->copy()->setTime(17, 0));
 
             $calendar->event($event);
         }
@@ -152,14 +119,14 @@ class ICalendarController extends Controller
 
     private function addAlternanceWeek($calendar, $weekStart)
     {
-        for ($dayOffset = 0; $dayOffset < 5; $dayOffset++) {
-            $currentDate = $weekStart->copy()->addDays($dayOffset);
+        for ($i = 0; $i < 5; $i++) {
+            $date = $weekStart->copy()->addDays($i)->setTimezone('Europe/Paris');
 
             $event = Event::create()
                 ->name('En alternance')
                 ->description('Période en entreprise')
-                ->startsAt($currentDate->copy()->setTime(8, 0))
-                ->endsAt($currentDate->copy()->setTime(19, 0));
+                ->startsAt($date->copy()->setTime(8, 0))
+                ->endsAt($date->copy()->setTime(19, 0));
 
             $calendar->event($event);
         }
