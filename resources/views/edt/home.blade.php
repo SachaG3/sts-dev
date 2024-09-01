a9b0a1352b8f2984d2deb94de1d776ccfba41149
Sacha Guignard <sacha.guignard@caensup.fr>
2024-09-02 00:24:37 +0200

    update speed edt/home

--- resources/views/edt/home.blade.php
+++ resources/views/edt/home.blade.php
@@ -56,8 +56,10 @@
         let calendar;
         const colors = [
             '#3788d8', '#ff9f89', '#66c2a5', '#fc8d62', '#8da0cb',
-            '#e78ac3', '#a6d854', '#ffd92f', '#e5c494', '#b3b3b3'
+            '#e78ac3', '#a6d854', '#ffd92f', '#e5c494', '#b3b3b3',
+            '#c8cbcd',
         ];
+
         const matiereColors = {};
         let colorIndex = 0;
         let calendarEvents = [];
