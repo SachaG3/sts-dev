ed2aa8f1cc9d84ff8874f46da324aa451e29d36a
SachaG3 <sacha.guignard@sts-sio-caen.info>
2024-09-27 18:56:41 +0200

    update speed edt/home

--- app/Models/Cours.php
+++ app/Models/Cours.php
@@ -6,10 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cours extends Model
 {
-    protected $fillable = ['jour_id', 'heure_debut', 'heure_fin', 'matiere', 'professeur', 'salle'];
+    protected $fillable = ['jour_id', 'heure_debut', 'heure_fin', 'matiere', 'matiere_id', 'professeur', 'salle'];
 
     public function jour()
     {
         return $this->belongsTo(Jour::class);
     }
+
+    public function matiere()
+    {
+        return $this->belongsTo(Matiere::class, 'matiere_id');
+    }
+
 }
+
